e2585771faded9010e14430c599f71d20b5ff5a2
neri-snappy <neri@snappy.com>
2022-05-25 22:31:13 +0300

    remove unused param

--- tests/rules/no-commonjs-module-exports.test.ts
+++ tests/rules/no-commonjs-module-exports.test.ts
@@ -2,7 +2,8 @@
 // this rule tests the new lines, which prettier will want to fix and break the tests
 /* eslint "@typescript-eslint/internal/plugin-test-formatting": ["error", { formatWithPrettier: false }] */
 /* eslint-enable eslint-comments/no-use */
-import rule, * as noCommonJsModuleExports from '../../src/rules/no-commonjs-module-exports';
+import rule from '../../src/rules/no-commonjs-module-exports';
+
 import { RuleTester } from '../RuleTester';
 
 const ruleTester = new RuleTester({
