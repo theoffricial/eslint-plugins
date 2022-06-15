fd19f2a4cc8d0b7f49aacf943fa2f430772a518f
unicop <its.op.the.unicorn@gmail.com>
2022-06-16 00:09:31 +0300

    insert tests threshold

--- jest.config.js
+++ jest.config.js
@@ -2,14 +2,29 @@
 /* eslint-disable import/unambiguous */
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
+    preset: 'ts-jest/presets/js-with-ts',
     globals: {
         'ts-jest': {
             tsconfig: 'tsconfig.test.json',
         },
     },
-    preset: 'ts-jest/presets/js-with-ts',
     testEnvironment: 'node',
-    modulePathIgnorePatterns: ['/build/'],
-    coveragePathIgnorePatterns: ['/build/'],
+    modulePathIgnorePatterns: ['build', 'website', 'node_modules'],
+    coveragePathIgnorePatterns: ['/build/', 'node_modules', 'website'],
     testRegex: './tests/.+\\.test\\.ts$',
+    collectCoverageFrom: [
+        '<rootDir>/src/**/*.{js,ts}',
+        '!<rootDir>/node_modules',
+        '!<rootDir>/website',
+    ],
+    collectCoverage: true,
+    coverageThreshold: {
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        'src/rules/**/*.ts': {
+            lines: 85,
+            branches: 80,
+            functions: 40,
+            statements: -10,
+        },
+    },
 };
