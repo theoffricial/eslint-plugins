58d1428ae88e8e50c47adb177a8829742a34feb3
unicop <its.op.the.unicorn@gmail.com>
2022-07-09 09:55:33 +0300

    new eslint-plugin-lib

---
+++ packages/eslint-plugin/README.md
@@ -0,0 +1,11 @@
+# eslint-plugin
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test eslint-plugin` to execute the unit tests via [Jest](https://jestjs.io).
+
+## Running lint
+
+Run `nx lint eslint-plugin` to execute the lint via [ESLint](https://eslint.org/).
--- packages/m2ts/src/index.ts
+++ packages/m2ts/src/index.ts
@@ -1,9 +1,22 @@
+import type { ESLint } from 'eslint';
 import { CRules } from './rules';
-import testConfig from './configs/js-with-ts';
+import nodeJsWithTs from './configs/node-js-with-ts';
+import nodeTsOnly from './configs/node-ts-only';
 
-export = {
-    rules: CRules,
+type TValidConfigs = 'node-js-with-ts' | 'node-ts-only';
+
+type TM2tsConfigs = {
+    [key in TValidConfigs]: ESLint.ConfigData;
+};
+
+type TM2tsPlugin = Omit<ESLint.Plugin, 'configs'> & { configs: TM2tsConfigs };
+
+const config: Required<Pick<TM2tsPlugin, 'configs' | 'rules'>> = {
+    rules: CRules as never,
     configs: {
-        'js-with-ts': testConfig,
+        'node-js-with-ts': nodeJsWithTs,
+        'node-ts-only': nodeTsOnly,
     },
 };
+
+export = config;
